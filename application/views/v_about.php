573b2863bc6a6cf78486ad4e243c16b066d5ac2c
renaldihardiansah <renaldihardiansah83@gmail.com>
2022-10-03 16:07:40 +0700

    pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Renaldi</li>
+        <li>Nama Belakang : Hardiansah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Cibarusah</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kuningan</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Catur</li>
+    </ul>
+</section>
